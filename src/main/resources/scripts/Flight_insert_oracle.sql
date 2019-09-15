e5712110b700ecd4d6fb8d3f78a2a3d5b54f1060
Narendra <mnarendra333@gmail.com>
2019-09-15 13:04:21 +0530

    added sql script for flight insertion

---
+++ src/main/resources/scripts/Flight_insert_oracle.sql
@@ -0,0 +1,30 @@
+insert into flight values(1,'AA1','American Airlines','AUS',
+'NYC','05-FEB-2018','05-FEB-2018 03:14:07');
+
+insert into flight values(2,'AA2','American Airlines','AUS',
+'NYC','05-FEB-2018','05-FEB-2018 05:14:07');
+
+insert into flight values(3,'AA3','American Airlines','AUS',
+'NYC','05-FEB-2018','05-FEB-2018 06:14:07');
+
+insert into flight values(4,'SW1','South West','AUS',
+'NYC','05-FEB-2018','05-FEB-2018 07:14:07');
+
+insert into flight values(5,'UA1','United Airlines','NYC',
+'DAL','05-FEB-2018','05-FEB-2018 10:14:07');
+
+insert into flight values(6,'UA1','United Airlines','NYC',
+'DAL','05-FEB-2018','05-FEB-2018 10:14:07');
+
+insert into flight values(7,'SW1','South West','AUS',
+'NYC','06-FEB-2018','06-FEB-2018 07:14:07');
+
+insert into flight values(8,'SW2','South West','AUS',
+'NYC','06-FEB-2018','06-FEB-2018 08:14:07');
+
+
+insert into flight values(9,'SW3','South West','NYC',
+'DAL','06-FEB-2018','06-FEB-2018 10:14:07');
+
+insert into flight values(10,'UA1','United Airlines','NYC',
+'DAL','06-FEB-2018','06-FEB-2018 10:14:07');
